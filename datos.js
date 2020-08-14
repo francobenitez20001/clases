1e6260c7cf0ef6d49b02b255b41b104ec12dc2e2
Franco Benitez <fhbenitez@hcdn.gob.ar>
2020-08-14 12:40:22 -0300

    update

---
+++ datos.js
@@ -0,0 +1,25 @@
+//tipos de datos
+
+//definicion de int
+let numero = 40;
+
+//definicion de strings
+let nombre = "Franco";
+
+//definicion de boolean
+let esMayor = true //false
+
+//definicion nulls
+let esNull = null;
+
+//definicion de undefined
+let unde = undefined;
+
+//se declaran en mayusculas
+const DNI = 43902208;
+
+
+//console.log(numero,nombre,DNI);
+
+
+//Declarar 3 variables y una constante que van hacer referencia al nombre,apellido, edad y DNI de una persona y luego las van a mostrar por consola.
